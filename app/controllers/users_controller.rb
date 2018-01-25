80d9e13bfcbf85cdf97148e5d607a16d8bd60ae9
tnpriyanka12 <tnpriyanka12@gmail.com>
2018-01-25 13:55:13 +1100

    Photo upload for users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,39 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create user_params
 
-    if user.persisted?
-      # user successfully created, redirect to profile page for this user
-      session[:user_id] = user.id
-      redirect_to user_path user
-    else
-      # error creating user (probably a validation error)
-      flash[:errors] = user.errors.full_messages
-      redirect_to new_user_path
+    user = User.new(user_params)
+    if params[:file].present?
+      req = Cloudinary::Uploader.upload(params[:file])
+      user.profile_pic = req["public_id"]
+      # raise 'hell'
     end
+    user.save
+    redirect_to user
 
+
+    # user = User.create user_params
+    # if user.persisted?
+    #   # user successfully created, redirect to profile page for this user
+    #   session[:user_id] = user.id
+    #   redirect_to user_path user
+    # else
+    #   # error creating user (probably a validation error)
+    #   flash[:errors] = user.errors.full_messages
+    #   redirect_to new_user_path
+    # end
   end
+
 
   def edit
     @user = User.find params[:id]
   end
+
+
+  def update
+
+  end
+
 
   def destroy
   end
